d2b65d06c23c4ecc549f249780400bc578a41759
lucasnogueirabra <lucasgcnogueira@gmail.com>
2017-08-14 10:30:58 -0300

    Add files via upload

---
+++ Listas_Sala/Listas_Sala/aula02/v0/Person.java
@@ -0,0 +1,29 @@
+package v0;
+
+
+public class Person {
+	int id;
+	static int nextId=0;
+	Person(){
+		id = nextId++;
+	}
+	
+	
+	   
+	public int getId() {
+	  return id;
+	}
+	
+
+
+}
+
+
+
+
+
+
+
+
+
+
